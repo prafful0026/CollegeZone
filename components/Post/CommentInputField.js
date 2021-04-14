5eaf78497cc5da46f8a845ffc99ed774afabc5ad
prafful0026 <prafful0026@gmail.com>
2021-04-15 00:54:56 +0530

    added comment add/delete functionality

--- components/Post/CommentInputField.js
+++ components/Post/CommentInputField.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { Form } from "semantic-ui-react";
+import {postComment} from "../../utils/postActions.js" 
 const CommentInputField = ({ postId, user, setComments }) => {
   const [text, setText] = useState("");
   const [loading, setLoading] = useState(false);
 
+
   return (
-    <Form reply>
+    <Form reply onSubmit={async (e)=>{e.preventDefault();
+    setLoading(true)
+    await postComment(postId,user,text,setComments,setText)
+    setLoading(false) }}>
       <Form.Input
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={(e) => {setText(e.target.value)}}
         placeholder='add comment'
         action={{
              color:"blue",
--- components/Post/PostComments.js
+++ components/Post/PostComments.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Comment, Icon } from "semantic-ui-react";
 import calculateTime from "../../utils/calculateTime.js";
+import { deleteComment } from "../../utils/postActions.js";
 
 const PostComments = ({ comment, user, setComments, postId }) => {
   const [disabled, setDisabled] = useState(false);
@@ -12,15 +13,25 @@ const PostComments = ({ comment, user, setComments, postId }) => {
         <Comment>
           <Comment.Avatar src={comment.user.profilePicUrl} />
           <Comment.Content>
-            <Comment.Author as='a' href={`/${comment.user.username}`} >{comment.user.name}</Comment.Author>
-            <Comment.Metadata>
-              {calculateTime(comment.date)}
-            </Comment.Metadata>
+            <Comment.Author as='a' href={`/${comment.user.username}`}>
+              {comment.user.name}
+            </Comment.Author>
+            <Comment.Metadata>{calculateTime(comment.date)}</Comment.Metadata>
             <Comment.Text>{comment.text}</Comment.Text>
             <Comment.Actions>
               <Comment.Action>
-               {(user.role==="admin"||comment.user._id===user._id)&&<Icon disabled={disabled} color="red" name="trash" />}
-
+                {(user.role === "admin" || comment.user._id === user._id) && (
+                  <Icon
+                    disabled={disabled}
+                    color='red'
+                    name='trash'
+                    onClick={async () => {
+                      setDisabled(true);
+                        deleteComment(postId, comment._id, setComments);
+                        setDisabled(false);
+                    }}
+                  />
+                )}
               </Comment.Action>
             </Comment.Actions>
           </Comment.Content>
