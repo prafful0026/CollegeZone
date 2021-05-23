7acc8f2bfc6f00ff47bf1b53ffa06e9af861614a
prafful0026 <prafful0026@gmail.com>
2021-05-23 23:06:18 +0530

    s

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://franzoned.herokuapp.com";
+    : "https://frandzoned.herokuapp.com";
 
 export default baseUrl;
